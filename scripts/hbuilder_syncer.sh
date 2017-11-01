61b83c827501f267f5ef52a552ef07c94ab14b62
Eddie_Mac <edward.wuxianru@gmail.com>
2017-11-01 15:59:12 -0400

    react added for hbuilder

---
+++ scripts/hbuilder_syncer.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ../web/dist/app/index.html ../../Hbuilder/infoBro
+cp ../web/dist/app/*.js ../../Hbuilder/infoBro/js
+cp ../web/dist/app/*.css ../../Hbuilder/infoBro/css
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,21 +1,4 @@
-#!/bin/bash
-
-# https://blog.madewithenvy.com/getting-started-with-webpack-2-ed2b86c68783
-
-# install yarn package control
-# override hadooop yarn
-brew link --overwrite yarn
-brew install yarn
-brew upgrade yarn
-mkdir ../web && cd ../web
-# install dependencies
-yarn init --yes
-yarn add --dev webpack webpack-dev-server babel-core babel-loader babel-preset-react babel-preset-es2015 \
-eslint flow css-loader less less-loader html-webpack-plugin source-map extract-text-webpack-plugin
-yarn add react react-dom react-router redux redux-thunk redux-promise react-redux 
-yarn install
-
-echo 'const path = require('path');
+const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
@@ -76,23 +59,4 @@ module.exports = {
       allChunks: true,
     }),    
   ],
-};' >> webpack.config.js
-
-mkdir -p dist/app
-mkdir -p src/components && mkdir -p src/styles && cd src
-
-# the html template for webpack html-loader
-echo '<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-  </head>
-  <body>
-    <div id="app"></div>
-  </body>
-</html>' >> index.tpl.html
-touch index.js
-
-# TODO: set up Eslint
-
+};
